08db2ccf06335c8e97bcb3bfefe8390fd60632d5
Jonathan <jonathanb2br@gmail.com>
2016-04-05 22:02:07 -0300

    correção de bugs

--- notebook3.jl
+++ notebook3.jl
@@ -1,15 +1,29 @@
 
 # Aqui iremos implementar o  o cálcula da FFT usando julia
 vetorFftJulia=[1:1:4096];
-@time fft(vetorFftJulia); 
+@time fft(vetorFftJulia);
 
-
-# criando a função myFFT, proposta para o trabalho
-function myfft(x) 
+# criando a função myIFFT, proposta para o trabalho
+function myifft(x) 
+    N=4096
+    par=Complex32[1:1:N]; 
+    impar=Complex32[1:1:N];
     N=4096    
     for i=1:N 
-        x[i]= cos(2*pi*i/N)-1im*sin(2*pi*i/N)
+        if i%2 == 0 
+            par[i]=x[i] 
+        elseif i%2 == 1 
+            impar[i]=x[i]
+        end
+    end 
+    N1=N/2
+    for i=1:N1 
+        par[i]=par[i]*exp(-1im*2*pi*i/N1) + exp(-1im*pi/N)        
+        for j=1:N1 
+            impar[j]=par[i]*impar[j]*exp(-1im*2*pi*i/N1) 
         end 
+    end
+    
 end
 
 N=4096 
@@ -22,10 +36,25 @@ vetorIfftJulia=[1:1:4096];
 
 # criando a função myIFFT, proposta para o trabalho
 function myifft(x) 
+    N=4096
+    par=Complex32[1:1:N]; 
+    impar=Complex32[1:1:N];
     N=4096    
     for i=1:N 
-        x[i]=1/N*cos(2*pi*i/N)-1im*sin(2*pi*i/N)
+        if i%2 == 0 
+            par[i]=x[i] 
+        elseif i%2 == 1 
+            impar[i]=x[i]
+        end
+    end 
+    N1=N/2
+    for i=1:N1 
+        par[i]=1/N*par[i]*exp(-1im*2*pi*i/N1) + exp(-1im*pi/N)        
+        for j=1:N1 
+            impar[j]=1/N*par[i]*impar[j]*exp(-1im*2*pi*i/N1) 
         end 
+    end
+    
 end
 
 N=4096 
